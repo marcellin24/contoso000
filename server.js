9d65dedf0c07402d5481bb78c508d6408f13731e
marcellin24 <84274973+marcellin24@users.noreply.github.com>
2023-05-15 14:57:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Azure App Service!');
+  res.end('Hello Marcellin, Azure App Service!');
 });
 
 server.listen(port, () => {
